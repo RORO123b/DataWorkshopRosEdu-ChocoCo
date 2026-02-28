2c0d243588c95b75e8f5f4910fbba0ddf607de5f
Robert Barbulescu <robert.cristian.barbulescu@gmail.com>
2026-02-28 11:51:23 +0200

    Cleaned data

--- src/clean_data.py
+++ src/clean_data.py
@@ -6,10 +6,9 @@ df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
 
 # print(pd.to_datetime(df['date'], dayfirst=True, errors='coerce').isna().sum())
 
-df['date'] = pd.to_datetime(df['date'], dayfirst=True, errors='coerce')
+df['date'] = pd.to_datetime(df['date'], dayfirst=True)
 df['amount'] = pd.to_numeric(df['amount'].str.replace('$', '').str.replace(',',''), errors='coerce')
 df['amount'] = df['amount'].dropna()
 df = df.drop_duplicates()
 
-print(df.head())
 df.to_csv('data/processed/Chocolate_Sales_Processed.csv', index=False)
